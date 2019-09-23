f191b8c60231b549e8ba3972d62166bae4906c17
Nikki <nikki@hackhouse.ca>
2019-09-22 17:40:35 -0700

    capture params from user curl call

--- lookupApp.py
+++ lookupApp.py
@@ -1,13 +1,20 @@
 import web
+import re
 
 urls = (
-    '/', 'index'
+    '/urlinfo/1/(.+?):(\d+)/(.+)', 'lookup'
 )
 
-class index: 
-    def GET(self): 
-        return "Hello World"
-
+class lookup: 
+    def GET(self, hostname, port, original_path_and_query_string): 
+        print hostname + " " + port + original_path_and_query_string
+        return 1
 if __name__ == "__main__": 
 	app = web.application(urls, globals())
 	app.run()
+
+
+
+#	curl http://urlinfo.com/{hostname_and_port}/{original_path_and_query_string} 
+
+
