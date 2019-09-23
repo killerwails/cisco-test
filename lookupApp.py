038f769d48cfed87fcb13d46fe8f274af44bd161
Nikki <nikki@hackhouse.ca>
2019-09-22 19:03:17 -0700

    successful db connection and query

--- lookupApp.py
+++ lookupApp.py
@@ -1,5 +1,15 @@
 import web
 import re
+import MySQLdb
+
+db = web.database(
+    dbn='mysql',
+    host='127.0.0.1',
+    user='lookupService',
+    pw='q8776G~2BYf67',
+    db='cisco',
+)
+
 
 urls = (
     '/urlinfo/1/(.+?):(\d+)/(.+)', 'lookup'
@@ -7,8 +17,10 @@ urls = (
 
 class lookup: 
     def GET(self, hostname, port, original_path_and_query_string): 
-        print hostname + " " + port + original_path_and_query_string
-        return 1
+        vars = dict(hostname=hostname,port=port,url=original_path_and_query_string)
+        results = db.query('select * from mockData where hostname=$hostname and port=$port and url=$url', vars=vars)
+        return results
+
 if __name__ == "__main__": 
 	app = web.application(urls, globals())
 	app.run()
