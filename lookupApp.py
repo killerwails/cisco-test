27016a9e335359b769b7a828bf9515c5d5798c32
Nikki <nikki@hackhouse.ca>
2019-09-23 21:11:23 -0700

    error handling added

--- lookupApp.py
+++ lookupApp.py
@@ -10,19 +10,32 @@ db = web.database(
     db='cisco',
 )
 
-
 urls = (
     '/urlinfo/1/(.+?):(\d+)/(.+)', 'lookup'
 )
 
-class lookup: 
-    def GET(self, hostname, port, original_path_and_query_string): 
-        from pprint import pprint
-        vars = dict(hostname=hostname,port=port,url=original_path_and_query_string+"?"+ web.ctx.environ['QUERY_STRING'])
+success_msg = "DANGEROUS\n"
+fail_msg = "OK\n"
+
+def getUrl(original_path_and_query_string): 
+    if web.ctx.env.get('QUERY_STRING'):
+        return original_path_and_query_string+"?"+ web.ctx.environ['QUERY_STRING']
+    else: 
+        return original_path_and_query_string
+
+def askDB(hostname,port,url):
+        vars = dict(hostname=hostname,port=port,url=url)
         results = db.query('select count(*) as matches from mockData where hostname=$hostname and port=$port and url=$url', vars=vars)
         matches = results[0].matches
-#        print env['QUERY_STRING']
         return matches
+
+class lookup: 
+    def GET(self, hostname, port, original_path_and_query_string): 
+        url = getUrl(original_path_and_query_string)
+        if askDB(hostname,port,url):
+        	return success_msg
+        else: 
+        	return fail_msg
 
 if __name__ == "__main__": 
 	app = web.application(urls, globals())
---
+++ tests/test-lookupApp.py
@@ -0,0 +1,3 @@
+import unittest
+
+class TestLookupMethods(unittest.TestCase)
